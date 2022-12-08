fee28b431d9da7dcdb25ab7c5e5bf21717880351
Julius <116170466+diplocentrus@users.noreply.github.com>
2022-12-08 12:35:55 +0100

    Add files via upload

---
+++ RachnerZahlsystemen/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace RachnerZahlsystemen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cmb1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmb1
+            // 
+            this.cmb1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.cmb1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cmb1.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.cmb1.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmb1.FormattingEnabled = true;
+            this.cmb1.Items.AddRange(new object[] {
+            "Dezimal zu Binär",
+            "Binär zu Dezimal",
+            "Dezimal zu Oktal",
+            "Oktal zu Dezimal",
+            "Dezimal zu Hexadezimal",
+            "Hexadezimal zu Dezimal"});
+            this.cmb1.Location = new System.Drawing.Point(288, 173);
+            this.cmb1.Name = "cmb1";
+            this.cmb1.Size = new System.Drawing.Size(283, 28);
+            this.cmb1.TabIndex = 0;
+            this.cmb1.Text = "Wählen Sie bitte Converter!";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuText;
+            this.button1.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.Location = new System.Drawing.Point(405, 244);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 53);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "BERECHNEN";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(14, 173);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBox1.Size = new System.Drawing.Size(259, 25);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(31, 363);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 35);
+            this.label1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(33, 333);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "ERGEBNIS";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(14, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(433, 144);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(33, 244);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 19);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "EINGABE";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Window;
+            this.label4.Location = new System.Drawing.Point(31, 277);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 35);
+            this.label4.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(680, 435);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cmb1);
+            this.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "juZIus                              Rechner von Zahlsystemen";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmb1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ RachnerZahlsystemen/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RachnerZahlsystemen
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            this.ActiveControl = textBox1;
+            textBox1.Focus();
+        }
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                textBox1.Focus();   
+            }
+            
+        }
+        private void label1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                label1.Focus();
+
+            }
+        }
+        
+
+
+
+        private void button1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1.PerformClick();
+                
+
+            } 
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (cmb1.SelectedIndex != 0 && cmb1.SelectedIndex != 1 && cmb1.SelectedIndex != 2
+                    && cmb1.SelectedIndex != 3 && cmb1.SelectedIndex != 4 && cmb1.SelectedIndex != 5)
+                {
+                    int inhalt = int.Parse(textBox1.Text);
+                    
+                    textBox1.Clear();
+                    label4.Text = "Wählen Sie bitte Converter!";
+                }
+
+                else if (cmb1.SelectedIndex != 1 && cmb1.SelectedIndex != 2
+                    && cmb1.SelectedIndex != 3 && cmb1.SelectedIndex != 4 && cmb1.SelectedIndex != 5)
+                {
+                    int inhalt = int.Parse(textBox1.Text);
+                    string v = Convert.ToString(inhalt, 2);
+                    label1.Text = v;
+                    textBox1.Clear();
+                    label4.Text = inhalt.ToString();
+                }
+                else if (cmb1.SelectedIndex != 0 && cmb1.SelectedIndex != 2
+                    && cmb1.SelectedIndex != 3 && cmb1.SelectedIndex != 4 && cmb1.SelectedIndex != 5)
+                {
+                    String inhalt = textBox1.Text;
+                    string inhalt2 = Convert.ToInt64(inhalt, 2).ToString();
+                    label1.Text = inhalt2;
+                    label4.Text = inhalt.ToString();
+                    textBox1.Clear();
+                }
+
+                else if (cmb1.SelectedIndex != 0 && cmb1.SelectedIndex != 1
+                    && cmb1.SelectedIndex != 3 && cmb1.SelectedIndex != 4 && cmb1.SelectedIndex != 5)
+                {
+                    String DecimalNumber = textBox1.Text;
+                    int i, j, Octal = 0, dn;
+                    int DecimalNumber2 = Convert.ToInt32(DecimalNumber);
+                    dn = DecimalNumber2;
+                    i = 1;
+                    for (j = DecimalNumber2; j > 0; j = j / 8)
+                    {
+                        Octal = Octal + (j % 8) * i;
+                        i *= 10;
+                        DecimalNumber2 /= 8;
+                    }
+                    label1.Text = Convert.ToString(Octal);
+                    textBox1.Clear();
+                    label4.Text = DecimalNumber.ToString();
+                }
+                else if (cmb1.SelectedIndex != 0 && cmb1.SelectedIndex != 1
+                    && cmb1.SelectedIndex != 2 && cmb1.SelectedIndex != 4 && cmb1.SelectedIndex != 5)
+                {
+                    int inhalt = int.Parse(textBox1.Text);
+                    int Decimal_Number = 0;
+                    int BASE = 1;
+                    int temp = inhalt;
+                    while (temp > 0)
+                    {
+                        int last_digit = temp % 10;
+                        temp /= 10;
+                        Decimal_Number += last_digit * BASE;
+                        BASE *= 8;
+                    }
+                    label1.Text = Convert.ToString(Decimal_Number);
+                    label4.Text = inhalt.ToString();
+                    textBox1.Clear();
+
+                }
+                else if (cmb1.SelectedIndex != 0 && cmb1.SelectedIndex != 1
+                    && cmb1.SelectedIndex != 2 && cmb1.SelectedIndex != 3 && cmb1.SelectedIndex != 5)
+                {
+
+                    int inhalt5 = int.Parse(textBox1.Text);
+                    int value = inhalt5;
+                    int toBase = 16;
+
+                    string hex = Convert.ToString(value, toBase);
+                    label1.Text = hex;
+                    label4.Text = inhalt5.ToString();
+                    textBox1.Clear();
+                }
+                else if (cmb1.SelectedIndex != 0 && cmb1.SelectedIndex != 1
+                    && cmb1.SelectedIndex != 2 && cmb1.SelectedIndex != 3 && cmb1.SelectedIndex != 4)
+                {
+                    String hex2 = textBox1.Text;
+                    int dez = Int32.Parse(hex2, System.Globalization.NumberStyles.HexNumber);
+                    label1.Text = Convert.ToString(dez);
+                    textBox1.Clear();
+                    label4.Text = hex2.ToString();
+                }
+                
+
+            }
+            catch(Exception ex) {
+                label1.Text = ex.Message;
+                textBox1.Clear();
+
+            }
+
+        
+        }
+
+
+    }
+
+
+        
+
+        
+    
+}
+
+
+
